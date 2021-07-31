b9298c0a0b7123c75ffcf7656e7b93daaea021c2
codeway-0841 <joincoin.codeway@gmail.com>
2021-07-31 18:04:47 +0500

    move mockdata to connector

--- src/Article/articleService.ts
+++ src/Article/articleService.ts
@@ -2,36 +2,11 @@
 import * as cheerio from 'cheerio'
 
 // internal
-import { randomText, randomIds } from '../connectors/mockData'
 import { BaseService } from '../connectors/baseService'
-import { testSize as userTestSize } from '../User/userService'
-
-// start of test data ->
-export const testSize = 50
-
-const createTestArticle = (id: string) => ({
-  id,
-  form: ['article', 'course'][Math.round(Math.random())],
-  authorId: randomIds(1, userTestSize)[0],
-  title: randomText(5).join(' '),
-  cover: 'im a test cover',
-  tags: randomText(3),
-  upstreamId: randomIds(1, testSize)[0],
-  downstreamIds: randomIds(5, testSize),
-  relatedArticleIds: randomIds(10, testSize),
-  MAT: Math.round(Math.random() * 100),
-  timestamp: new Date().toISOString(),
-  pinnedCommentIds: [],
-  subscriberIds: randomIds(5, userTestSize), // should be moved to action table
-  publishState: 'published'
-})
-
-const items = [...Array(testSize).keys()].map(i => createTestArticle(String(i)))
-// <- end of test data
-
+import { randomText } from '../connectors/mockData/utils'
 export class ArticleService extends BaseService {
   constructor() {
-    super(items)
+    super('article')
   }
 
   countWords = (html: string) =>
@@ -39,12 +14,12 @@ export class ArticleService extends BaseService {
       .load(html)('body')
       .text()
       .split(' ')
-      .filter(s => s !== '').length
+      .filter((s) => s !== '').length
 
   // TODO: replaced by actual dynamoDB api
   // start of db calls ->
   findByAuthor = (id: Array<string>) =>
-    new Promise(resolve =>
+    new Promise((resolve) =>
       resolve(this.items.filter(({ authorId }) => id === authorId))
     )
   // <- end of db calls
